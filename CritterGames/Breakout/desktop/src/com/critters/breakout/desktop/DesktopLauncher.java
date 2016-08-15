ca1d9a266e19428fe336169283b10cdc53655698
Žiga Vene <zvene28@gmail.com>
2016-08-15 17:25:57 +0200

    Minesweeper First Commit (#4)
    * Minesweeper First Commit
    
    First commit of Minesweeper. Most game mechanics already work. Baisc
    functionality implemented. Ready for first tests with HTML delpoyment.
    
    * Breakout Initial Commit
    
    Created the project files for the game Breakout. The example
    auto-generated display works.

---
+++ CritterGames/Breakout/desktop/src/com/critters/breakout/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.critters.breakout.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.critters.breakout.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
