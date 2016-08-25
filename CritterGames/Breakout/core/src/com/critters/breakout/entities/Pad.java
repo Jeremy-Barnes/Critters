596217e35435a89df1d024cf80e5fd943cb0a248
Žiga Vene <zvene28@gmail.com>
2016-08-25 17:03:07 +0200

    Minigame dev (#29)
    * Minesweeper First Commit
    
    First commit of Minesweeper. Most game mechanics already work. Baisc
    functionality implemented. Ready for first tests with HTML delpoyment.
    
    * Breakout Initial Commit
    
    Created the project files for the game Breakout. The example
    auto-generated display works.
    
    * Added basic classes
    
    Mostly coppied from Minesweeper.
    
    * Started Breakout
    
    Commit includes Websocket tests and the beginning of Breakout. Added
    basic classes for rendering balls and blocks, will work on collision
    next.
    
    * Breakout Physics
    
    Created the collision detection needed for the game.
    
    * Fixed collision
    
    There were a bunch of problems with collision. Should be now fixed ..
    
    * Added Paddle and Walls
    
    Added paddle and walls. Bouncing next.
    
    * Added bouning of the Ball from the Pad, Walls and Blocks
    
    * Improved Pad bouncing
    
    Major performance improvement. Including the renderer and the collision
    detector (bricks color when they are in range of collision detection,
    debug feature).
    Ball now controllable with pad.
    
    * Fixed JS size
    
    Game playable.
    
    * Improved rendering
    
    Major improvement with JS
    
    * Minesweeper screen size
    
    Changed the Minesweeper screen size, it now fits, no more white border.
    
    Also added a reset with the key 'R', temporary.
    
    * Resize fix
    
    Not also works in HTML
    
    * Removed Floor
    
    Removed the bottom wall, was a debug feature. Also implemented reset
    with key R - temporary probably.
    
    * Fixed Paddle and Ball
    
    Paddle now had a maximum speed and cannot teleport. It can also not go
    past the screen.
    
    Ball should now not get trapped due to a countdown timer for bounce
    implemented.  Ball can only bounce every 10 updates along each axis.
    (timing can be changed if needed)
    
    * Breakout Input Improvements
    
    Added Keyboard input.
    
    There are now 3 modes for input. First is following the pointer, second
    is going left or right depending on the press location on the screen.
    Third is keyboard.
    
    Might need to remove one of the mouse modes, more testing needed on
    mobile to confirm. Input might seem funny.

--- CritterGames/Breakout/core/src/com/critters/breakout/entities/Pad.java
+++ CritterGames/Breakout/core/src/com/critters/breakout/entities/Pad.java
@@ -1,21 +1,95 @@
 package com.critters.breakout.entities;
+
 import static com.critters.breakout.graphics.Render.sr;
+import static com.critters.breakout.Level.level;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.critters.breakout.Level;
 import com.critters.breakout.math.Vector2f;
 
 public class Pad extends Collidable {
 
+	enum InputMode {
+		MOUSE, POINTER, KEYBOARD;
+	}
+
+	// Movement of the pad
+	private float goalX;
+	private float velX;
+	private final float MAX_VEL = 5;
+
+	private InputMode mode;
+
 	public Pad(Vector2f pos, Vector2f size) {
 		super(pos, size);
+		rectangle.onlyBottom = true;
+
+		mode = InputMode.KEYBOARD;
+		goalX = pos.x;
+	}
+
+	private void processInput() {
+
+		// Switching between modes
+		if (Gdx.input.getDeltaX() != 0 || Gdx.input.getDeltaY() != 0) {
+			mode = InputMode.POINTER;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
+			mode = InputMode.KEYBOARD;
+		}
+
+		if (Gdx.input.isTouched() || Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+			mode = InputMode.MOUSE;
+		}
+
+		switch (mode) {
+		case POINTER:
+			goalX = Gdx.input.getX() - size.x / 2;
+			break;
+
+		case MOUSE:
+			if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) || Gdx.input.isTouched()) {
+				int x = Gdx.input.getX();
+				if (x < level.LEVEL_WIDTH / 2) {
+					goalX -= MAX_VEL;
+				} else {
+					goalX += MAX_VEL;
+				}
+			}
+			break;
+
+		case KEYBOARD:
+			if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D))
+				goalX += MAX_VEL;
+			if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A))
+				goalX -= MAX_VEL;
+			break;
+		}
 	}
 
 	@Override
 	public void update() {
-		pos.x = Gdx.input.getX() - size.x / 2;
+		processInput();
+
+		// Speed limitation
+		float deltaX = Math.abs(pos.x - goalX);
+		if (deltaX < MAX_VEL) {
+			pos.x = goalX;
+		} else {
+			pos.x += (pos.x - goalX) > 0 ? -MAX_VEL : MAX_VEL;
+		}
+
+		// Position limitation
+		if (pos.x < level.WALL_SIZE)
+			pos.x = level.WALL_SIZE;
+		if (pos.x + size.x > level.LEVEL_WIDTH - level.WALL_SIZE)
+			pos.x = level.LEVEL_WIDTH - level.WALL_SIZE - size.x;
+
+		// Collision update
 		rectangle.update(pos, pos.add(size));
 	}
 
