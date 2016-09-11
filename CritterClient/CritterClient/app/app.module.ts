ef15ac0f80a599236d488e0ed85885e9288930a3
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2016-09-11 00:38:14 -0500

    Add components that look a bit like old temp-page.

--- CritterClient/CritterClient/app/app.module.ts
+++ CritterClient/CritterClient/app/app.module.ts
@@ -1,9 +1,16 @@
 ﻿import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent }   from './components/app.component';
+import { AccountFormComponent }   from './components/accountform.component';
+
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [AppComponent, AccountFormComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ CritterClient/CritterClient/app/components/accountform.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component, Input } from '@angular/core';
+import { User } from '../dtos'
+
+@Component({
+    selector: 'acct-form',
+    templateUrl: '../../templates/accountform.template.htm'
+})
+
+export class AccountFormComponent {
+    @Input()
+    user: User;
+
+    confirmPassword: string;
+}
