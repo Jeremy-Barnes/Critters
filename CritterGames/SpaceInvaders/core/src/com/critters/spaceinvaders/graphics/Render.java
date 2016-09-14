3fb86af99ba08a3d4f93b96a69fd0170518aa37d
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2016-09-13 22:28:08 -0500

    Revert "Minigame dev (#96)"
    This reverts commit 4b9de43fa3a9fc1d1846518ab17f87d1ef34d96f.

--- CritterGames/SpaceInvaders/core/src/com/critters/spaceinvaders/graphics/Render.java
+++
@@ -1,7 +0,0 @@
-package com.critters.spaceinvaders.graphics;
-
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-
-public class Render {
-	public static ShapeRenderer sr = new ShapeRenderer();
-}
