9b7266ea0fe7b27224f92a586c5136878e479806
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2016-10-16 17:36:25 -0500

    Add some boilerplate for the account updater.

--- CritterClient/CritterClient/app/components/accountform.component.ts
+++ CritterClient/CritterClient/app/components/accountform.component.ts
@@ -1,15 +1,19 @@
 ﻿import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { User } from '../dtos'
 import {Application} from "../appservice"
 
 @Component({
-    selector: 'acct',
     templateUrl: '../../templates/accountform.template.htm'
 })
 
 export class AccountFormComponent {
     user: User;
     confirmPassword: string;
+
+    constructor(private router: Router) { }
+
 
     ngOnInit() { this.user = Application.user }
 
