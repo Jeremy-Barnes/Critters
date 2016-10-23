e3aba4309489c1f1a658ed56aa7d61ab20bae1a4
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2016-10-22 21:17:09 -0500

    Get pet options from server instead of test data.

--- CritterClient/CritterClient/app/appservice.ts
+++ CritterClient/CritterClient/app/appservice.ts
@@ -1,9 +1,11 @@
-﻿import { User, CreateAccountRequest, Pet } from './dtos'
+﻿import {User, Pet, PetColor, PetSpecies, CreateAccountRequest} from './dtos'
 import {ServiceMethods} from "./servicemethods"
 
 
 export class Application {
     public static user: User = new User();
+    public static petSpecies: PetSpecies[];
+    public static petColors: PetColor[];
 
     public static submitUserAccountCreationRequest(user: User, pet: Pet) : JQueryPromise<User> {
         var createRequest = new CreateAccountRequest();
@@ -20,14 +22,16 @@ export class Application {
         return ServiceMethods.changeUserInformation(user);
     }
 
-    public static getPetSpecies() {
-        //ServiceMethods.getPetOptions(createRequest);
-        return [{ petSpeciesConfigID: 1, petTypeName: "dog" }, { petSpeciesConfigID: 2, petTypeName: "cat" }, { petSpeciesConfigID: 3, petTypeName: "horrible clion" }]; //todo replace with server call, this is test data
+    public static getPetSpecies(): JQueryPromise<PetSpecies[]> {
+        var self = this;
+        return ServiceMethods.getPetSpecies().done((p: PetSpecies[]) => { self.petSpecies = p; });
+        //return [{ petSpeciesConfigID: 1, petTypeName: "dog" }, { petSpeciesConfigID: 2, petTypeName: "cat" }, { petSpeciesConfigID: 3, petTypeName: "horrible clion" }]; //todo replace with server call, this is test data
     }
 
-    public static getPetColors() {
-        //ServiceMethods.getPetOptions(createRequest);
-        return [{ petColorConfigID: 1, petColorName: "blue" }, { petColorConfigID: 2, petColorName: "red" }, { petColorConfigID: 3, petColorName: "octarine" }]; //todo replace with server call, this is test data
+    public static getPetColors(): JQueryPromise<PetColor[]>{
+        var self = this;
+        return ServiceMethods.getPetColors().done((p: PetColor[]) => { self.petColors = p; });
+        //return [{ petColorConfigID: 1, petColorName: "blue" }, { petColorConfigID: 2, petColorName: "red" }, { petColorConfigID: 3, petColorName: "octarine" }]; //todo replace with server call, this is test data
 
     }
 }
