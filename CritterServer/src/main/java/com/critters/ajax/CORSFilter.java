4c138c387bfdec16cf4b7a77f045100f69b0a6e4
Jeremy Barnes <Jeremiah.Barnes@outlook.com>
2016-10-28 12:16:39 -0500

    Internet says ajax withCreds doesnt allow origin *

--- CritterServer/src/main/java/com/critters/ajax/CORSFilter.java
+++ CritterServer/src/main/java/com/critters/ajax/CORSFilter.java
@@ -15,7 +15,7 @@ public class CORSFilter implements ContainerResponseFilter {
 	@Override
 	public void filter(ContainerRequestContext request,
 					   ContainerResponseContext response) throws IOException {
-		response.getHeaders().add("Access-Control-Allow-Origin", "*");
+		response.getHeaders().add("Access-Control-Allow-Origin", "http://f2197ada.ngrok.io");
 		response.getHeaders().add("Access-Control-Allow-Credentials", "true");
 		response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 		response.getHeaders().add("Access-Control-Allow-Headers",  "origin, content-type, accept, authorization, selectorvalidator, Cookie");
