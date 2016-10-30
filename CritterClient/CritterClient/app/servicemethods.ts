ad8f8cc1409178da2e1a9f4dcf8b943d4f60c641
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2016-10-30 15:00:52 -0500

    Add url rewriting for testing with cookies off.

--- CritterClient/CritterClient/app/servicemethods.ts
+++ CritterClient/CritterClient/app/servicemethods.ts
@@ -3,15 +3,14 @@
 import {User, Pet, PetColor, PetSpecies, CreateAccountRequest, Friendship} from './dtos'
 
 export class ServiceMethods {
-    static baseURL: string = "http://581c949b.ngrok.io/api/critters/";// "http://localhost:8080/api/critters/"; //"http://581c949b.ngrok.io/api/critters/";//"http://localhost:8080/api/critters/";
     static selectorValidator: string[];
-    static jsessionID: string;
+    static jsessionID: string = null;
 
     private static doAjax(functionName: string, functionService: string, parameters: any, type: string = "POST"): JQueryPromise<any> {
         var param = JSON.stringify(parameters);
         var pathParams = type == "GET" && parameters != null ? "/" + param : "";
         var settings: JQueryAjaxSettings = {
-            url: ServiceMethods.baseURL + functionService + "/" + functionName + pathParams,
+            url: ServiceMethods.baseURL + functionService + "/" + functionName + pathParams + (ServiceMethods.jsessionID != null ? ";jsessionid=" + ServiceMethods.jsessionID : ""),
             type: type,
             contentType: "application/json",
             xhrFields: {
