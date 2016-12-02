23a70d88a5a87bf8b4268a865532612f77e38fd2
Žiga Vene <zvene28@gmail.com>
2016-12-02 18:14:21 +0100

    Breakout Timing
    Fixed the issues with timing on different platforms. Now works like a
    charm. The system is now also more flexible in regadrs of speeds and
    timings. More precision!

--- CritterGames/Breakout/core/src/com/critters/breakout/entities/Ball.java
+++ CritterGames/Breakout/core/src/com/critters/breakout/entities/Ball.java
@@ -9,6 +9,7 @@ import static com.critters.breakout.math.Rectangle.VERTICAL;
 import java.util.ArrayList;
 import java.util.Random;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.critters.breakout.entities.blocks.Block;
 import com.critters.breakout.entities.blocks.BlockIndestructible;
@@ -27,7 +28,7 @@ public class Ball extends Entity {
 	private Circle circle;
 
 	private Vector2f vel;
-	private final float MAX_VEL_DEFAULT = 4;
+	private final float MAX_VEL_DEFAULT = 250; // Per sec
 	private float maxVel;
 
 	private boolean fireBall = false;
@@ -151,7 +152,7 @@ public class Ball extends Entity {
 	}
 
 	public void normalVel() {
-		vel = vel.normal().mul(maxVel);
+		vel = vel.normal().mul(maxVel * Gdx.graphics.getDeltaTime());
 	}
 
 	@Override
@@ -164,6 +165,7 @@ public class Ball extends Entity {
 		// Check for active powerups
 		checkActivePowerups();
 
+		normalVel();
 		pos = pos.add(vel);
 
 		circle.updateCenter(pos);
