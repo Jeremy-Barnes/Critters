d3251df19ae4b97aad263b0048a06216520042ce
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2016-12-03 21:17:03 -0600

    Cleanup db, extend hibernate entities (messages)

--- CritterServer/src/main/sql/12.03.16.01.message_extensions.sql
+++ CritterServer/src/main/sql/12.03.16.01.message_extensions.sql
@@ -1,3 +1,3 @@
 ALTER TABLE messages ALTER COLUMN dateSent TYPE TIMESTAMP WITH TIME ZONE;
-ALTER TABLE messages ADD COLUMN rootMessage int null REFERENCES messages(messageID);
-ALTER TABLE messages ADD COLUMN parentMessage int null REFERENCES messages(messageID);
+ALTER TABLE messages ADD COLUMN rootMessageId int null REFERENCES messages(messageID);
+ALTER TABLE messages ADD COLUMN parentMessageId int null REFERENCES messages(messageID);
