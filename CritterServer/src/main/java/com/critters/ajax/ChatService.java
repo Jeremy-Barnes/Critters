f40f5712976cef4c5dc520c4ba20044daa4af1d7
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2016-12-03 20:31:48 -0600

    Add basic message sending (DB not notifications)

---
+++ CritterServer/src/main/java/com/critters/ajax/ChatService.java
@@ -0,0 +1,39 @@
+package com.critters.ajax;
+
+import com.critters.bll.ChatBLL;
+import com.critters.dal.dto.entity.Message;
+import com.critters.dal.dto.entity.User;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBElement;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+
+/**
+ * Created by Jeremy on 11/29/2016.
+ */
+@Path("/chat")
+public class ChatService extends AjaxService {
+
+	@POST
+	@Path("/sendMessage")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response sendMessage(JAXBElement<Message> message) throws GeneralSecurityException, UnsupportedEncodingException {
+		User loggedInUser = (User) httpRequest.getSession().getAttribute("user");
+		if(loggedInUser == null) {
+			return Response.status(Response.Status.UNAUTHORIZED).entity("You need to log in first!").build();
+		}
+
+		Message msg = message.getValue();
+		msg = ChatBLL.sendMessage(msg, loggedInUser);
+		//todo send message to recipient as notification
+		return Response.status(Response.Status.OK).entity(msg).build();
+	}
+
+}
