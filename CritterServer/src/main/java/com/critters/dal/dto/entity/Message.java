6f0aef5fe1ec1a9e7623f1031efd92807c161ef2
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2016-12-03 20:31:48 -0600

    Add basic message sending (DB not notifications)

---
+++ CritterServer/src/main/java/com/critters/dal/dto/entity/Message.java
@@ -0,0 +1,103 @@
+package com.critters.dal.dto.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * Created by Jeremy on 8/14/2016.
+ */
+@Entity
+@Table(name="messages")
+public class Message {
+
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name="increment", strategy = "increment")
+	private int messageID;
+
+
+	@ManyToOne
+	@JoinColumn(name = "senderUserID")
+	private User sender;
+
+	@ManyToOne
+	@JoinColumn(name = "recipientUserID")
+	private User recipient;
+
+	private boolean read;
+	@Temporal(TemporalType.DATE)
+	private Date dateSent;
+
+	private String messageText;
+	private String messageSubject;
+
+	public Message(User sender, User recipient, boolean read, Date dateSent, String messageText, String messageSubject) {
+		this.sender = sender;
+		this.recipient = recipient;
+		this.read = read;
+		this.dateSent = dateSent;
+		this.messageText = messageText;
+		this.messageSubject = messageSubject;
+	}
+
+	public boolean isRead() {
+		return read;
+	}
+
+	public void setRead(boolean read) {
+		this.read = read;
+	}
+
+	public String getMessageText() {
+		return messageText;
+	}
+
+	public void setMessageText(String messageText) {
+		this.messageText = messageText;
+	}
+
+	public String getMessageSubject() {
+		return messageSubject;
+	}
+
+	public void setMessageSubject(String messageSubject) {
+		this.messageSubject = messageSubject;
+	}
+
+	public Message(){}
+
+	public int getMessageID() {
+		return messageID;
+	}
+
+	public void setMessageID(int friendshipID) {
+		this.messageID = friendshipID;
+	}
+
+	public User getSender() {
+		return sender;
+	}
+
+	public void setSender(User requester) {
+		this.sender = requester;
+	}
+
+	public User getRecipient() {
+		return recipient;
+	}
+
+	public void setRecipient(User requested) {
+		this.recipient = requested;
+	}
+
+	public Date getDateSent() {
+		return dateSent;
+	}
+
+	public void setDateSent(Date dateSent) {
+		this.dateSent = dateSent;
+	}
+}
