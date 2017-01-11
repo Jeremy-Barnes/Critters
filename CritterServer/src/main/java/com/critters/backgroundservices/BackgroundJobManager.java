79da9964df88a9507be74f461d9864f26279f94a
Jeremy Barnes <Jeremy-barnes@utulsa.edu>
2017-01-10 22:05:23 -0600

    more thorough logging

--- CritterServer/src/main/java/com/critters/backgroundservices/BackgroundJobManager.java
+++ CritterServer/src/main/java/com/critters/backgroundservices/BackgroundJobManager.java
@@ -12,6 +12,12 @@ public class BackgroundJobManager {
 	public static int jobs = 0;
 	public static String logs = "";
 
+	public static void printLine(String string){
+		logs = logs + " \n" + string + "\n\n\n next log \n\n\n";
+		System.out.println(string);
+
+	}
+
 	@Schedule(hour="*", minute="*", second="*/10", persistent=true)
 	public void someQuarterlyJob() {
 		jobs++;
