c95e2dc7b2a7b437c3dd6971ac3cc26e86a82032
Jeremy Barnes <Jeremy-barnes@utulsa.edu>
2017-01-10 22:21:41 -0600

    More logging

--- CritterServer/src/main/java/com/critters/backgroundservices/BackgroundJobManager.java
+++ CritterServer/src/main/java/com/critters/backgroundservices/BackgroundJobManager.java
@@ -2,6 +2,8 @@ package com.critters.backgroundservices;
 
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 /**
  * Created by Jeremy on 8/24/2016.
@@ -16,6 +18,13 @@ public class BackgroundJobManager {
 		logs = logs + " \n" + string + "\n\n\n next log \n\n\n";
 		System.out.println(string);
 
+	}
+
+	public static void printLine(Exception e){
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		e.printStackTrace(pw);
+		printLine(sw.toString());
 	}
 
 	@Schedule(hour="*", minute="*", second="*/10", persistent=true)
