a343f3c10a23a7e377a8da69144d9cd4a9f12515
Jeremy Barnes <Jeremiah.Barnes@outlook.com>
2017-01-15 22:41:07 -0600

    Merge in server work - messaging, long polling, commerce (#135)
    * Update Friendship to return object after request.
    
    * Add basic message sending (DB not notifications)
    
    * Upgrade message functionality in DB
    
    * Update Hibernate to match new DB for messages
    
    * Cleanup db, extend hibernate entities (messages)
    
    * Fix sql naming issue, this project will carry into 2017
    
    * Preliminary work for stores
    
    * Basic inventory work
    
    * More item stuff - inventory and store items.
    
    * Add a tiny bit of logging because of how weird tomee is
    
    * Fix bug whereby item buyers were obliterated
    
    Also add new store and inventory endpoints/functionality.
    
    * Finalize store operations
    
    * Purchasing from NPC shops should work now.
    
    * Add long polling and integrate w/ messages/friends

--- CritterServer/src/main/java/com/critters/ajax/CommerceService.java
+++ CritterServer/src/main/java/com/critters/ajax/CommerceService.java
@@ -2,6 +2,8 @@ package com.critters.ajax;
 
 import com.critters.bll.CommerceBLL;
 import com.critters.dal.dto.ItemRequest;
+import com.critters.dal.dto.entity.Store;
+
 import com.critters.dal.dto.entity.User;
 
 import javax.resource.spi.InvalidPropertyException;
@@ -45,6 +47,22 @@ public class CommerceService extends AjaxService {
 		if(loggedInUser == null) {
 			return Response.status(Response.Status.UNAUTHORIZED).entity("You need to log in first!").build();
 		} else {
+			CommerceBLL.changeItemStore(request.item, loggedInUser);
+			return Response.status(Response.Status.OK).build();
+		}
+	}
+
+
+	@POST
+	@Path("/setInventoryItemStorePrice")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response setInventoryItemStorePrice(JAXBElement<ItemRequest> jsonRequest) throws JAXBException, IOException, InvalidPropertyException {
+		User loggedInUser = (User) httpRequest.getSession().getAttribute("user");
+		ItemRequest request = jsonRequest.getValue();
+		if(loggedInUser == null) {
+			return Response.status(Response.Status.UNAUTHORIZED).entity("You need to log in first!").build();
+		} else {
 			CommerceBLL.changeItemPrice(request.item, loggedInUser);
 			return Response.status(Response.Status.OK).build();
 		}
@@ -62,6 +80,9 @@ public class CommerceService extends AjaxService {
 		} else {
 			request.item.setPrice(null);
 			CommerceBLL.changeItemPrice(request.item, loggedInUser);
+			request.item.setContainingStoreId(null);
+			CommerceBLL.changeItemStore(request.item, loggedInUser);
+
 			return Response.status(Response.Status.OK).build();
 		}
 	}
@@ -79,6 +100,44 @@ public class CommerceService extends AjaxService {
 			CommerceBLL.changeItemOwnerViaPurchase(request.item, loggedInUser);
 			return Response.status(Response.Status.OK).build();
 		}
+	}
+
+	@POST
+	@Path("/createStore")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createStore(JAXBElement<Store> jsonRequest) throws JAXBException, IOException, InvalidPropertyException {
+		User loggedInUser = (User) httpRequest.getSession().getAttribute("user");
+		Store request = jsonRequest.getValue();
+		if(loggedInUser == null) {
+			return Response.status(Response.Status.UNAUTHORIZED).entity("You need to log in first!").build();
+		} else {
+			return Response.status(Response.Status.OK).entity(CommerceBLL.createStore(request, loggedInUser)).build();
+		}
+	}
+
+	@POST
+	@Path("/editStore")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response editStore(JAXBElement<Store> jsonRequest) throws JAXBException, IOException, InvalidPropertyException {
+		User loggedInUser = (User) httpRequest.getSession().getAttribute("user");
+		Store request = jsonRequest.getValue();
+		if(loggedInUser == null) {
+			return Response.status(Response.Status.UNAUTHORIZED).entity("You need to log in first!").build();
+		} else {
+			return Response.status(Response.Status.OK).entity(CommerceBLL.editStore(request, loggedInUser)).build();
+		}
+	}
+
+	@POST
+	@Path("/getStorefront")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response viewStore(JAXBElement<Store> jsonRequest) throws JAXBException {
+		Store copiedStore = super.serializeDeepCopy(CommerceBLL.getStore(jsonRequest.getValue()), Store.class);
+		return Response.status(Response.Status.OK).entity(copiedStore).build();
+
 	}
 
 
--- CritterServer/src/main/java/com/critters/bll/ChatBLL.java
+++ CritterServer/src/main/java/com/critters/bll/ChatBLL.java
@@ -5,14 +5,44 @@ import com.critters.dal.dto.entity.Message;
 import com.critters.dal.dto.entity.User;
 
 import javax.persistence.EntityManager;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.TimeoutHandler;
+import javax.ws.rs.core.Response;
 import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * Created by Jeremy on 11/29/2016.
  */
 public class ChatBLL {
+
+	private static Map<Integer, AsyncResponse> listeners = Collections.synchronizedMap(new HashMap<Integer, AsyncResponse>());
+
+	public static void createPoll(int userId, AsyncResponse asyncResponse){
+		asyncResponse.setTimeoutHandler(new TimeoutHandler() {
+			@Override
+			public void handleTimeout(AsyncResponse asyncResponse) {
+				listeners.remove(userId);
+				asyncResponse.resume(Response.status(Response.Status.NOT_FOUND)
+											 .entity("Operation time out.").build());
+			}
+		});
+		asyncResponse.setTimeout(30, TimeUnit.SECONDS);
+		listeners.put(userId, asyncResponse);
+	}
+
+	public static void notify(int userId, Object notification){
+		if(listeners.containsKey(userId)) {
+			listeners.get(userId).resume(notification);
+			listeners.remove(userId);
+		}
+	}
 
 	public static Message sendMessage(Message message, User user) throws GeneralSecurityException, UnsupportedEncodingException {
 
@@ -26,7 +56,10 @@ public class ChatBLL {
 			entityManager.getTransaction().commit();
 			entityManager.refresh(mail);
 			entityManager.close();
-			return wipeSensitiveDetails(mail);
+			Message wiped = wipeSensitiveDetails(mail);
+			notify(message.getRecipient().getUserID(), wiped);
+			return wiped;
+
 		} else {
 			throw new GeneralSecurityException("Invalid cookie supplied");
 		}
