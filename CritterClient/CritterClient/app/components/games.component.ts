ad869a5235b12ec05967a18b4d1e268eaeec333f
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2017-02-26 01:48:11 -0600

    Maybe an iframe is the route to success?

--- CritterClient/CritterClient/app/components/games.component.ts
+++ CritterClient/CritterClient/app/components/games.component.ts
@@ -13,11 +13,20 @@ export class GamesComponent implements OnInit {
     activeGame: GameThumbnail = new GameThumbnail();
     games: GameThumbnail[];
     ngOnInit() { this.user = this.app.user; }
+    isFull = false;
 
     constructor(private route: ActivatedRoute) {
         Application.getGames();
         this.games = this.app.games;
+        var gth = new GameThumbnail();
+        gth.gameDescription = "sdfsd";
+        gth.gameName = "sdfsdf";
+        this.games.push(gth);
 
+    }
+
+    goFull() {
+        this.isFull = true;
     }
 
     selectGame(game: GameThumbnail) {
@@ -25,9 +34,10 @@ export class GamesComponent implements OnInit {
         //    jQuery(\"#gameBind\").html(html);
         //});
         this.activeGame = game;
-
+     //   var ifrm = document.createElement('iframe');
+        document.getElementById("gameBind").insertBefore
         jQuery('#gameBind').append("butts");
-        jQuery('#gameBind').append("<script src=\"http://409cf50d.ngrok.io/CritterGames/SpaceInvaders/html/html.nocache.js\"></script> <script src= \"Libraries/soundmanager2-setup.js\" ></script> <script type= \"text/javascript\" src= \"Libraries/soundmanager2-jsmin.js\" > </script><script> function handleMouseDown(evt) { evt.preventDefault(); evt.stopPropagation(); evt.target.style.cursor = 'default';} function handleMouseUp(evt) {evt.preventDefault();        evt.stopPropagation();       evt.target.style.cursor = '';   } document.getElementById('embed-html').addEventListener('mousedown', handleMouseDown, false);document.getElementById('embed-html').addEventListener('mouseup', handleMouseUp, false);</script>");
+       // jQuery('#gameBind').append("<iframe><script src=\"dist/html/html.nocache.js\"></script> <script src= \"Libraries/soundmanager2-setup.js\" ></script> <script type= \"text/javascript\" src= \"Libraries/soundmanager2-jsmin.js\" > </script><script> function handleMouseDown(evt) { evt.preventDefault(); evt.stopPropagation(); evt.target.style.cursor = 'default';} function handleMouseUp(evt) {evt.preventDefault();        evt.stopPropagation();       evt.target.style.cursor = '';   } document.getElementById('embed-html').addEventListener('mousedown', handleMouseDown, false);document.getElementById('embed-html').addEventListener('mouseup', handleMouseUp, false);</script>");
     }
 
 }
