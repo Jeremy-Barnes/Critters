7655fe3cea4be24a5833351352cc4e38392f8263
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2017-03-04 18:27:15 -0600

    Fix silly requirements on images sql

--- CritterServer/src/main/sql/2017.02.26.01.add_image_fields_and_message_options.sql
+++ CritterServer/src/main/sql/2017.02.26.01.add_image_fields_and_message_options.sql
@@ -4,14 +4,14 @@ ALTER TABLE petSpeciesConfigs ADD COLUMN speciesDescription varchar(2000) not nu
 ALTER TABLE messages ADD COLUMN showRecipient boolean not null;
 ALTER TABLE messages ADD COLUMN showSender boolean not null;
 ALTER TABLE petColorConfigs ADD COLUMN patternPath varchar(200) not null;
-ALTER TABLE storeConfigs add column storeClerkImagePath varchar(200) not null;
-ALTER TABLE storeConfigs add column storeBackgroundImagePath varchar(200) not null;
-ALTER TABLE users add column userImagePath varchar(200) not null;
+ALTER TABLE storeConfigs add column storeClerkImagePath varchar(200) null;
+ALTER TABLE storeConfigs add column storeBackgroundImagePath varchar(200) null;
+ALTER TABLE users add column userImagePath varchar(200) null;
 ALTER TABLE users drop column birthdate;
 ALTER TABLE users add column birthMonth int not null;
 ALTER TABLE users add column birthDay int not null;
-ALTER TABLE users add constraint monthCheck CHECK (birthMonth <= 12 AND birthMonth >= 1)
-ALTER TABLE users add constraint dayCheck CHECK (birthDay <= 31 AND birthDay >= 1)
+ALTER TABLE users add constraint monthCheck CHECK (birthMonth <= 12 AND birthMonth >= 1);
+ALTER TABLE users add constraint dayCheck CHECK (birthDay <= 31 AND birthDay >= 1);
 
 CREATE TABLE userImageOptions(
     userImageOptionID SERIAL NOT NULL PRIMARY KEY,
