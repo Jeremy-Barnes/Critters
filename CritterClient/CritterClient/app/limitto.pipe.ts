119f6580b38137d3445ad20fcb97ea860e8320d8
Jeremy Barnes <Jeremiah.Barnes@outlook.com>
2017-06-15 20:13:06 -0500

    Html dev (#187)
    * Fix the build by using new properties
    
    * Fix sex radio buttons
    
    * Fix other sex radio and remove testing stuff.
    
    * Fix popover
    
    * Remove weird pointless method
    
    * Fix message behaviors (subject, compose-send)
    
    * Fix double result error
    
    * try to fix notification logic
    
    * Fix servicemethods bug and make popover work well
    
    * Fix autocomplete by adding Comparable interface.
    
    Types!
    Hell yeah! Now we got business!
    
    * Get notifications at login
    
    * Fix notifications popup?
    
    * Try to fix issue with limitto
    
    * CASING

--- CritterClient/CritterClient/app/limitto.pipe.ts
+++ CritterClient/CritterClient/app/limitto.pipe.ts
@@ -5,7 +5,9 @@ export class LimitToPipe implements PipeTransform {
     transform(value: string, args: string): string {
         let limit = args ? parseInt(args, 10) : 10;
         let trail = '...';
+        if(value)
+            return value.length > limit ? value.substring(0, limit) + trail : value;
+        return "";
 
-        return value.length > limit ? value.substring(0, limit) + trail : value;
     }
 }
