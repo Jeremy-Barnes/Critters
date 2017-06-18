809298c387fe296eabfbb2b35d32bf84533d9b46
Jeremy Barnes <Jeremiah.Barnes@outlook.com>
2017-06-18 17:01:49 -0500

    Build server crash

--- CritterServer/src/main/java/com/critters/ajax/CORSFilter.java
+++ CritterServer/src/main/java/com/critters/ajax/CORSFilter.java
@@ -15,7 +15,7 @@ public class CORSFilter implements ContainerResponseFilter {
 	@Override
 	public void filter(ContainerRequestContext request,
 					   ContainerResponseContext response) throws IOException {
-		response.getHeaders().add("Access-Control-Allow-Origin", "http://142f42a8.ngrok.io");
+		response.getHeaders().add("Access-Control-Allow-Origin", "http://0c7a5243.ngrok.io");
 		response.getHeaders().add("Access-Control-Allow-Credentials", "true");
 		response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 		response.getHeaders().add("Access-Control-Allow-Headers",  "origin, content-type, accept, authorization, selectorvalidator, Cookie, Content-Type, critters");
