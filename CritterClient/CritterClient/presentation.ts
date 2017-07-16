e1efe460e2ddd43f6a681876b326b7665826e404
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2017-07-15 19:11:01 -0500

    Make mobile search work, make mobile sidebar go

--- CritterClient/CritterClient/presentation.ts
+++ CritterClient/CritterClient/presentation.ts
@@ -2,11 +2,23 @@
 //jQuery(document).ready(
 
 function prepDisplay() {
-
-
     //off-canvas stuff
     jQuery('[data-toggle="offcanvas"]').click(function () {
         jQuery('.sidebar-offcanvas').toggleClass('active')
+    });
+
+    //hide collapses when not in use
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $("#userMenuMobile").hasClass("in");
+        if (_opened === true && !clickover.hasClass("buttonCollapse")){
+            $("#userMenuClickMobile").click();
+        }
+
+        _opened = $("#userMenu").hasClass("in");
+        if (_opened === true && !clickover.hasClass("buttonCollapse")){
+            $("#userMenuClick").click();
+        }
     });
 
     //other stuff here later
