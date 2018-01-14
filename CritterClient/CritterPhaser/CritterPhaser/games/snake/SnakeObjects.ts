97104c998231b543dea713b2db6d506d077c18d1
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2018-01-14 09:36:29 -0600

    Classes aren't hoisted, so rearrange.
    https://github.com/Microsoft/TypeScript/issues/10837

--- CritterClient/CritterPhaser/CritterPhaser/games/snake/SnakeObjects.ts
+++ CritterClient/CritterPhaser/CritterPhaser/games/snake/SnakeObjects.ts
@@ -21,7 +21,76 @@ export class SnakeFood extends GameObject {
 
 }
 
-export class SnakeHead extends GameObject {
+
+export class SnakeTail extends GameObject {
+    sprite: Phaser.Sprite;
+    game: Phaser.Game;
+
+    next: SnakeTail;
+    prev: SnakeTail;
+    destination: Phaser.Point;
+    inflects = [];
+
+    constructor(game: Phaser.Game, x, y, sprite: string) {
+        super();
+        this.game = game;
+        this.x = x; this.y = y; this.xVector = -1; this.yVector = 0;
+        this.sprite = game.add.sprite(0, 0, sprite ? sprite : 'body');
+        this.sprite.anchor.set(0.5, 0.5);
+        this.sprite.x = this.x;
+        this.sprite.y = this.y;
+        game.world.add(this.sprite);
+    }
+
+    getPoint() {
+        return this.inflects.length > 0 ? this.inflects.shift() : null;
+    }
+
+    update(dt: number, head: SnakeHead) {
+
+        this.x += 200 * dt * this.xVector;
+        this.y += 200 * dt * this.yVector;
+        this.sprite.x = this.x;
+        this.sprite.y = this.y;
+        if (this.destination == null || Phaser.Math.fuzzyEqual(this.x, this.destination.x, 200 * dt * .5) && Phaser.Math.fuzzyEqual(this.y, this.destination.y, 200 * dt * .5)) {
+            if (this.destination != null) {
+                this.inflects.push(this.destination);
+                this.x = this.destination.x;
+                this.y = this.destination.y;
+            }
+            this.destination = this.prev.getPoint();
+            if (this.destination != null) {
+                this.xVector = this.destination.x - this.x;
+                this.xVector = this.xVector / Math.abs(this.xVector == 0 ? 1 : this.xVector);
+                this.yVector = this.destination.y - this.y;
+                this.yVector = this.yVector / Math.abs(this.yVector == 0 ? 1 : this.yVector);
+            } else {
+                this.destination = new Phaser.Point(this.prev.x, this.prev.y);
+                this.xVector = this.prev.xVector;
+                this.yVector = this.prev.yVector;
+            }
+        }
+
+        if (this.next != null) {
+            this.next.update(dt, head);
+        }
+    }
+
+    addBody() {
+        if (this.next) {
+            this.next.addBody();
+        } else {
+            var newtail = new SnakeTail(this.game, this.x - this.xVector * 50, this.y - this.yVector * 50, null);
+            newtail.xVector = this.xVector;
+            newtail.yVector = this.yVector;
+            this.next = newtail;
+            newtail.prev = this;
+        }
+    }
+}
+
+
+export class SnakeHead extends SnakeTail {
     sprite: Phaser.Sprite;
     body: SnakeTail[] = [];
     game: Phaser.Game
@@ -30,7 +99,7 @@ export class SnakeHead extends GameObject {
     next: SnakeTail;
 
     constructor(game: Phaser.Game, x, y) {
-        super();
+        super(game,x,y, '');
         this.game = game;
         this.x = x; this.y = y; this.xVector = -1; this.yVector = 0;
         this.sprite = game.add.sprite(0, 0, 'head');
@@ -61,7 +130,7 @@ export class SnakeHead extends GameObject {
 
     update(dt: number) {
         if (this.next)
-            this.next.update(dt);
+            this.next.update(dt, this);
         this.x += 200 * dt * this.xVector;
         this.y += 200 * dt * this.yVector;
         this.sprite.x = this.x;
@@ -88,72 +157,6 @@ export class SnakeHead extends GameObject {
         }
     }
     
-}
-
-export class SnakeTail extends GameObject {
-    sprite: Phaser.Sprite;
-    game: Phaser.Game;
-
-    next: SnakeTail;
-    prev: SnakeTail;
-    destination: Phaser.Point;
-    inflects = [];
-
-    constructor(game: Phaser.Game, x, y, sprite: string) {
-        super();
-        this.game = game;
-        this.x = x; this.y = y; this.xVector = -1; this.yVector = 0;
-        this.sprite = game.add.sprite(0, 0, sprite ? sprite : 'body');
-        this.sprite.anchor.set(0.5, 0.5);
-        this.sprite.x = this.x;
-        this.sprite.y = this.y;
-        game.world.add(this.sprite);
-    }
-
-    getPoint() {
-        return this.inflects.length > 0 ? this.inflects.shift() : null;
-    }     
-
-    update(dt : number) {
-        this.x += 200 * dt * this.xVector;
-        this.y += 200 * dt * this.yVector;
-        this.sprite.x = this.x;
-        this.sprite.y = this.y;
-        if (this.destination == null || Phaser.Math.fuzzyEqual(this.x, this.destination.x, 200 * dt * .5) && Phaser.Math.fuzzyEqual(this.y, this.destination.y, 200 * dt * .5)) {
-            if (this.destination != null) {
-                this.inflects.push(this.destination);
-                this.x = this.destination.x;
-                this.y = this.destination.y;
-            }
-            this.destination = this.prev.getPoint();
-            if (this.destination != null) {
-                this.xVector = this.destination.x - this.x;
-                this.xVector = this.xVector / Math.abs(this.xVector == 0 ? 1 : this.xVector);
-                this.yVector = this.destination.y - this.y;
-                this.yVector = this.yVector / Math.abs(this.yVector == 0 ? 1 : this.yVector);
-            } else {
-                this.destination = new Phaser.Point(this.prev.x, this.prev.y);
-                this.xVector = this.prev.xVector;
-                this.yVector = this.prev.yVector;
-            }
-        }
-
-        if (this.next != null) {
-            this.next.update(dt);
-        }
-    }
-
-    addBody() {
-        if (this.next) {
-            this.next.addBody();
-        } else {
-            var newtail = new SnakeTail(this.game, this.x - this.xVector * 50, this.y - this.yVector * 50, null);
-            newtail.xVector = this.xVector;
-            newtail.yVector = this.yVector;
-            this.next = newtail;
-            newtail.prev = this;
-        }
-    }
 }
 
 export class Obstacle extends GameObject {
