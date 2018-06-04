0babe50bb247c5a262f7e6bb182b910d433ca291
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2018-06-03 21:13:10 -0500

    First cut at quests table

--- CritterServer/src/main/java/com/critters/ajax/WorldService.java
+++ CritterServer/src/main/java/com/critters/ajax/WorldService.java
@@ -19,7 +19,7 @@ public class WorldService extends AjaxService {
 	@Path("/actionNPC/{npcID}/{action}/{targetIdAmt}")
 	@UserSecure
 	@GET
-	public Response actionToNPC(@PathParam("npcID") int npcID, @PathParam("action") int actionCode, @PathParam("targetIdAmt") int target) throws InterruptedException {
+	public Response actionToNPC(@PathParam("npcID") int npcID, @PathParam("action") int actionCode, @PathParam("targetIdAmt") int target) {
 		User loggedInUser = getSessionUser();
 		NPCResponse response = WorldBLL.actionNPC(npcID, actionCode, target, loggedInUser);
 		return Response.status(200).entity(response).build();
@@ -28,7 +28,7 @@ public class WorldService extends AjaxService {
 	@Path("/NPC/{npcID}")
 	@UserSecure
 	@GET
-	public Response getNPC(@PathParam("npcID") int npcID) throws InterruptedException {
+	public Response getNPC(@PathParam("npcID") int npcID) {
 		User loggedInUser = getSessionUser();
 		NPCResponse response = WorldBLL.getNPC(npcID, loggedInUser);
 		return Response.status(200).entity(response).build();
--- CritterServer/src/main/sql/2018.01.07.01.npcs_and_game_scoring.sql
+++ CritterServer/src/main/sql/2018.01.07.01.npcs_and_game_scoring.sql
@@ -8,3 +8,23 @@ CREATE TABLE npcs(
 
 ALTER TABLE gameThumbnailConfigs ADD COLUMN pointToCurrencyFactor int;
 ALTER TABLE gameThumbnailConfigs ADD COLUMN scoreHandlerScript TEXT;
+
+CREATE TABLE questLines(
+    questID SERIAL NOT NULL PRIMARY KEY,
+    startStep INT NOT NULL REFERENCES questStep(questStepID),
+);
+
+CREATE TABLE questSteps(
+    questStepID SERIAL NOT NULL PRIMARY KEY,
+    previousStep INT NOT NULL REFERENCES questStep(questStepID),
+    nextStep INT NOT NULL REFERENCES questStep(questStepID),
+    giver INT NOT NULL REFERENCES npcs(npcID),
+    actionHandlerScript TEXT
+);
+
+CREATE TABLE userQuestStepInstance(
+    userQuestStepInstanceID SERIAL NOT NULL PRIMARY KEY,
+    questUserID INT NOT NULL REFERENCES users(questStepID),
+    currentQuestStepID INT NOT NULL REFERENCES questSteps(questStepID),
+    startedDate DATE NOT NULL
+);
