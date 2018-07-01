4f68297d913669a5ddd8c16a41f3c0e524132fab
Jeremy Barnes <jeremy-barnes@utulsa.edu>
2018-06-30 20:51:27 -0500

    Code cleanup + random fetchquest handling complete

--- CritterServer/src/main/java/com/critters/bll/WorldBLL.java
+++ CritterServer/src/main/java/com/critters/bll/WorldBLL.java
@@ -10,6 +10,7 @@ import com.critters.dal.entity.QuestInstance;
 import com.critters.dal.entity.User;
 import com.critters.dto.NPCResponse;
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,9 +18,8 @@ import org.slf4j.LoggerFactory;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import javax.script.ScriptException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -56,62 +56,115 @@ public class WorldBLL {
 						response.subItems = (NPCResponse.NPCMessageItem[]) ((ScriptObjectMirror) responseObject.get("subItems")).values().toArray(new NPCResponse.NPCMessageItem[0]);
 					}
 
-
 					if (actionCode == NPCActions.SubmitQuest) {
-						usersQuest = dal.quests.getQuestInstance(targetIdOrAmt);
-						if(usersQuest != null && usersQuest.getCompletedDate() == null && (usersQuest.getRandomQuestGiverRedeemer() == npcID ||
-						(usersQuest.getCurrentStep() != null && usersQuest.getCurrentStep().getRedeemerNPC() != null &&
-						usersQuest.getCurrentStep().getRedeemerNPC().getNpcID() == npcID))) {
-
-							if(usersQuest.getRandomQuestObjectivesJSONObject() != null) {
-								boolean insufficientGifts = false;
-								JSONObject jsonObj = Serializer.dictionaryFromJSON(usersQuest.getRandomQuestObjectivesJSONObject());
-
-								JSONObject retrieveItems = (JSONObject) jsonObj.get("giveItems");
-								if(retrieveItems != null) {
-									loggedInUser.setInventory(UserBLL.getUserInventory(loggedInUser.getUserID()));
-									List<Item> items = loggedInUser.getInventory().stream().filter(i -> itemIDs.contains(i.getInventoryItemId())).collect(Collectors.toList());
-									Map<String, Object> ItemsAndQtys = retrieveItems.toMap();
-									for (Map.Entry<String, Object> itemQty : ItemsAndQtys.entrySet()) {
-										int itemID = Integer.parseInt(itemQty.getKey());
-										int qty = Integer.parseInt(itemQty.getValue().toString());
-										if(items.stream().filter(i -> i.getDescription().getItemConfigID() == itemID).count() < qty) {
-											insufficientGifts = true;
-										}
-									}
-									UserBLL.discardInventoryItems((Item[])items.toArray(), loggedInUser, null);
-								} //fetch quest
-
-							}//random quest objectives
-
-						}
+						response = handleSubmitQuest(dal, engine, inv, npcID, targetIdOrAmt, loggedInUser, itemIDs, response);
+					} else if(actionCode == NPCActions.SayHello) {
+						response = handleHello(dal, engine, inv, npc, loggedInUser, response);
 					}
-
-					//append eligible new quests to the NPC response
-					List<QuestInstance.StoryQuestStep> eligibleQuests = null;
-					if (actionCode == NPCActions.SayHello) {
-						eligibleQuests = checkForNewQuests(npc.getNpcID(), loggedInUser);
-					}
-					if (!Extensions.isNullOrEmpty(eligibleQuests)) {
-						response.questItems = new NPCResponse.NPCQuestMessage[eligibleQuests.size()];
-						for (int i = 0; i < eligibleQuests.size(); i++) {
-							Object questScript = engine.eval(eligibleQuests.get(i).getActionHandlerScript());
-							String tagLine = (String) inv.invokeFunction("getTagline", loggedInUser);
-							String imagePath = (String) inv.invokeFunction("getImage", loggedInUser);
-							int questStepID = eligibleQuests.get(i).getStoryQuestStepID();
-							String questText = (String) inv.invokeFunction("getQuestDescription", loggedInUser);
-							String questTitle = eligibleQuests.get(i).getQuestTitle();
-							response.questItems[i] = new NPCResponse.NPCQuestMessage(tagLine, imagePath, questStepID, null);
-						}
-					}
-
-
-				}
+				}//can do action
 			} catch (Exception ex) {
 				logger.error("Couldn't execute the NPC js for npc " + npcID, ex);
 			}
 			return response;
 		}
+	}
+
+	private static NPCResponse handleHello(DAL dal, ScriptEngine engine, Invocable inv, NPC npc, User loggedInUser,NPCResponse response) throws ScriptException, NoSuchMethodException {
+		List<QuestInstance.StoryQuestStep> eligibleQuests = null;
+		eligibleQuests = checkForNewQuests(dal, npc.getNpcID(), loggedInUser);
+		if (!Extensions.isNullOrEmpty(eligibleQuests)) {
+			response.questItems = new NPCResponse.NPCQuestMessage[eligibleQuests.size()];
+			for (int i = 0; i < eligibleQuests.size(); i++) {
+				Object questScript = engine.eval(eligibleQuests.get(i).getActionHandlerScript());
+				String tagLine = (String) inv.invokeFunction("getTagline", loggedInUser);
+				String imagePath = (String) inv.invokeFunction("getImage", loggedInUser);
+				int questStepID = eligibleQuests.get(i).getStoryQuestStepID();
+				String questText = (String) inv.invokeFunction("getQuestDescription", loggedInUser);
+				String questTitle = eligibleQuests.get(i).getQuestTitle();
+				response.questItems[i] = new NPCResponse.NPCQuestMessage(tagLine, imagePath, questStepID, null);
+			}
+		}
+		return response;
+	}
+
+
+	private static NPCResponse handleSubmitQuest(DAL dal, ScriptEngine engine, Invocable inv,
+												 int activeNPCID, int targetIdOrAmt, User loggedInUser, List<Integer> itemIDs, NPCResponse npcResponse)
+			throws ScriptException, NoSuchMethodException {
+		QuestInstance usersQuest = dal.quests.getQuestInstance(targetIdOrAmt);
+		if(usersQuest != null && usersQuest.getCompletedDate() == null) {
+			if(usersQuest.getCurrentStep() != null) {
+				handleSubmitStoryQuest(usersQuest, dal, engine, inv, activeNPCID, targetIdOrAmt, loggedInUser, itemIDs);
+			} else if(usersQuest.getRandomQuestObjectivesJSONObject() != null) {
+				npcResponse.questItems = new NPCResponse.NPCQuestMessage[1];
+				npcResponse.questItems[0] = handleSubmitRandomQuest(usersQuest, dal, activeNPCID, targetIdOrAmt, loggedInUser, itemIDs);
+			}
+		} else {
+
+		}
+		return npcResponse;
+	}
+
+	private static NPCResponse.NPCQuestMessage handleSubmitRandomQuest(QuestInstance usersQuest, DAL dal, int activeNPCID, int targetIdOrAmt, User loggedInUser, List<Integer> itemIDs){
+		NPCResponse.NPCQuestMessage response = new NPCResponse.NPCQuestMessage();
+
+		JSONObject jsonObj = Serializer.dictionaryFromJSON(usersQuest.getRandomQuestObjectivesJSONObject());
+		JSONObject retrieveItems = (JSONObject) jsonObj.get("giveItems");//TODO formalize all this as a class
+
+		if(retrieveItems != null) {
+			List<Item> outParamFilteredItems = new ArrayList<Item>();
+			Random rand = new Random();
+
+			if(sufficientGifts(loggedInUser, itemIDs, retrieveItems, outParamFilteredItems)) {
+				if(UserBLL.discardInventoryItems((Item[]) outParamFilteredItems.toArray(), loggedInUser, null)) {
+					dal.beginTransaction();
+					usersQuest.setCompletedDate(new Date());
+					dal.quests.save(usersQuest);
+					dal.commitTransaction();
+					JSONArray successResponses = jsonObj.getJSONArray("successResponses");
+					response.messageText = successResponses.get(rand.nextInt(successResponses.length())).toString();
+				}
+			} else {
+				JSONArray failureResponses = jsonObj.getJSONArray("incompleteFailureResponses");
+				response.messageText = failureResponses.get(rand.nextInt(failureResponses.length())).toString();
+
+			}
+		}
+		return response;
+	}
+
+	private static void handleSubmitStoryQuest(QuestInstance usersQuest, DAL dal, ScriptEngine engine, Invocable inv, int activeNPCID,
+											   int targetIdOrAmt, User loggedInUser, List<Integer> itemIDs) throws ScriptException, NoSuchMethodException {
+
+		if(usersQuest.getCurrentStep().getRedeemerNPC() != null && usersQuest.getCurrentStep().getRedeemerNPC().getNpcID() == activeNPCID){
+			Object questScript = engine.eval(usersQuest.getCurrentStep().getActionHandlerScript());
+			String tagLine = (String) inv.invokeFunction("getTagline", loggedInUser);
+
+		} else {
+
+		}
+	}
+
+
+	private static boolean sufficientGifts(User loggedInUser, List<Integer> itemIDs, JSONObject npcWantItems, List<Item> outParamFilteredItems) {
+		boolean insufficientGifts = true;
+		if(!Extensions.isNullOrEmpty(itemIDs)) {
+			loggedInUser.setInventory(UserBLL.getUserInventory(loggedInUser.getUserID()));
+			outParamFilteredItems.addAll(loggedInUser.getInventory().stream().filter(i -> itemIDs.contains(i.getInventoryItemId())).collect(Collectors.toList()));
+			Map<String, Object> ItemsAndQtys = npcWantItems.toMap();
+			for (Map.Entry<String, Object> itemQty : ItemsAndQtys.entrySet()) {
+				int itemConfigID = Integer.parseInt(itemQty.getKey());
+				int qty = Integer.parseInt(itemQty.getValue().toString());
+				if (outParamFilteredItems.stream().filter(i -> i.getDescription().getItemConfigID() == itemConfigID).count() >= qty) {
+					insufficientGifts = false;
+				} else {
+					insufficientGifts = true;
+					break;
+				}
+			}
+		}
+		outParamFilteredItems.add(new Item());
+		return !insufficientGifts;
 	}
 
 	public static NPCResponse getNPC(int npcID, User loggedInUser) {
@@ -128,24 +181,23 @@ public class WorldBLL {
 		return response;
 	}
 
-	private static List<QuestInstance.StoryQuestStep> checkForNewQuests(int npcID, User loggedInUser){
+	private static List<QuestInstance.StoryQuestStep> checkForNewQuests(DAL dal, int npcID, User loggedInUser){
 		ArrayList<QuestInstance.StoryQuestStep> userQuests = new ArrayList<QuestInstance.StoryQuestStep>();
-		try(DAL dal = new DAL()) {
-			List<QuestInstance.StoryQuestStep> rootQuests = dal.quests.getStoryQuestStepConfigByGiverID(npcID, true);
-			for(QuestInstance.StoryQuestStep quest : rootQuests){
-				try {
-					ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript"); //TODO newing this up N times is stupid
-					//but making this thread static causes function name collisions. Need to centralize and cache scripted values.
-					Invocable inv = (Invocable) engine;
-					engine.eval(quest.getActionHandlerScript());
-					if((boolean)inv.invokeFunction("isEligible", loggedInUser)){
-						userQuests.add(quest);
-					}
-				} catch(Exception ex){
-					logger.error("Couldn't execute the quest js for npc " + npcID + " and quest " + quest.getStoryQuestStepID(), ex);
+		List<QuestInstance.StoryQuestStep> rootQuests = dal.quests.getStoryQuestStepConfigByGiverID(npcID, true);
+		for(QuestInstance.StoryQuestStep quest : rootQuests){
+			try {
+				ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript"); //TODO newing this up N times is stupid
+				//but making this thread static causes function name collisions. Need to centralize and cache scripted values.
+				Invocable inv = (Invocable) engine;
+				engine.eval(quest.getActionHandlerScript());
+				if((boolean)inv.invokeFunction("isEligible", loggedInUser)){
+					userQuests.add(quest);
 				}
+			} catch(Exception ex){
+				logger.error("Couldn't execute the quest js for npc " + npcID + " and quest " + quest.getStoryQuestStepID(), ex);
 			}
 		}
+
 		return userQuests;
 	}
 
